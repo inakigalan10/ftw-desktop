1f9862477e48fe7a2afeca08025598994ee556e3
Iñaki Galan Mata <inakigalan17@gmail.com>
2023-04-28 20:19:42 +0200

    matching

--- ftw-desktop/src/Profile/Profile.jsx
+++ ftw-desktop/src/Profile/Profile.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import './Profile.css';
+import { BsFillPencilFill } from 'react-icons/bs';
+
 
 const Profile = () => {
+  const [showeditProfileClick, seteShowditProfileClick] = useState(false);
+
+  const editProfileClick = () => {
+    seteShowditProfileClick(true);
+    // Aquí podrías agregar más lógica relacionada al like
+  };
   return (
-    <div>Profile</div>
+    <div> 
+      <div className='header_profile'>
+        <div className='img_profile'>
+          <img src="./img/img-user.jpg" alt="User avatar" />
+        </div>
+        <div className='info_perfil'>
+          <div className='name_profile'>
+            <h1 className="user_name_profile">John Doe</h1>
+          </div>
+          <div className='username_profile'>
+            <p className="user_email_profile">@fhkdsjdj</p>
+        </div>
+        </div>
+       <div className='acciones_profile'>
+            <div className='edit_perfile' onClick={editProfileClick}>
+                <BsFillPencilFill/>
+            </div>
+
+       </div>
+      </div>
+      {showeditProfileClick && (
+        <div>
+          <div className="pop-up-profile">
+            Té ha gustado el perfil 
+            ¿Habra suerte?
+          
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
--- ftw-desktop/src/matching/Matching.jsx
+++ ftw-desktop/src/matching/Matching.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './matching.css';
+import { AiFillLike } from 'react-icons/ai';
+import { AiFillDislike } from 'react-icons/ai';
 
 const Matching = () => {
-  return (
-    <div>matching</div>
-  )
-}
+  const [showPopUpLike, setShowPopUpLike] = useState(false);
+  const [ShowPopUpDislike, setShowPopUpDislike] = useState(false);
 
-export default Matching
+  const handleLikeClick = () => {
+    setShowPopUpLike(true);
+    // Aquí podrías agregar más lógica relacionada al like
+  };
+
+  const handleDislikeClick = () => {
+    setShowPopUpDislike(true);
+    // Aquí podrías agregar más lógica relacionada al dislike
+  };
+
+  return (
+    <div className='main'>
+      <div className='like_user' onClick={handleLikeClick}>
+        <AiFillLike />
+      </div>
+      <div className='card'>
+        <div className='img-card'>
+          <img src='./img/avatar.jpeg' alt='' />
+        </div>
+        <div className='name-user-card'>
+          <h1>@Pepitopalotes</h1>
+        </div>
+        <div className='info-user-card'>
+          <div className='horario-user-card'>
+            <p>noches 23:00h-02:00h</p>
+          </div>
+          <div className='desc-user-card'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              error ab voluptatem ut ipsa neque voluptas blanditiis repellat!
+              Aliquam totam vero amet fuga praesentium in saepe nesciunt
+              accusamus. Aliquid, aut?
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='no_like_user' onClick={handleDislikeClick}>
+        <AiFillDislike />
+      </div>
+      {showPopUpLike && (
+        <div>
+          <div className="pop-up">
+            Té ha gustado el perfil 
+            ¿Habra suerte?
+          <div className="progress"></div>
+          </div>
+          <div className="progress-bar">
+            <div className="progress"></div>
+          </div>
+        </div>
+      )}
+      {ShowPopUpDislike && (
+        <div>
+          <div className="pop-up-Dislike">
+          No té ha gustado el perfil 
+            ¡¡¡¡Pobrecillo!!!!!
+          <div className="progress"></div>
+          </div>
+          <div className="progress-bar">
+            <div className="progress"></div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Matching;
